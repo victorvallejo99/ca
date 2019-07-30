d74f2cf0965701dd289e64e897d7e502513564ff
areizas <mateo.areiza@quind.io>
2019-07-30 17:55:17 -0500

    Project structure init

---
+++ src/main/java/io/quind/basiccalculatorapi/BasicCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package io.quind.basiccalculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCalculatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicCalculatorApiApplication.class, args);
+    }
+
+}
